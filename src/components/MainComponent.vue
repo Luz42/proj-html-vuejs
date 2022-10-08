d76f00f621a71aa2c4ef13f277c7f7ea6c4d5a0c
Luz42 <a.evangelista93@gmail.com>
2022-10-08 15:14:10 +0200

    Implementata la prima sezione "band-news" in main
    layout di base ok

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,6 +1,38 @@
 <template>
   <main>
+    <section id="Band-news">
     <SectionsTitlesComponent title="latest band news"/>
+    <div class="container-xl pb-5">
+      <div class="row">
+        <div class="col-8">
+          <div class="row">
+            <div class="col-12 mb-4">
+              <img class="img-fluid" src="@/assets/images/blog_music_techo-1200x600.jpg" alt="">
+            </div>
+            <div class="col-6">
+              <img class="img-fluid" src="@/assets/images/blog-post3-600x900.jpg" alt="">
+            </div>
+            <div class="col-6">
+              <img class="img-fluid" src="@/assets/images/blog-post4-600x900.jpg" alt="">
+            </div>
+            <div class="col-12 mt-4">
+              <img class="img-fluid" src="@/assets/images/blog_flavor_rock-1200x600.jpg" alt="">
+            </div>
+          </div>
+        </div>
+        <div class="col-4 d-flex">
+          <div class="row gap-4">
+            <div class="col-12">
+              <img style="height:100%; object-fit: cover;" class="img-fluid" src="@/assets/images/blog-post1-600x900.jpg" alt="">
+            </div>
+            <div class="col-12">
+              <img style="height:100%; object-fit: cover;" class="img-fluid" src="@/assets/images/blog-post2-600x900.jpg" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
   </main>
 </template>
 
