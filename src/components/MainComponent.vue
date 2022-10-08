c26624b085db82f4588174103396f891bb4c7577
Luz42 <a.evangelista93@gmail.com>
2022-10-08 18:59:26 +0200

    Implementato il layout generico di un card

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -7,7 +7,16 @@
         <div class="col-8">
           <div class="row">
             <div class="col-12 mb-4">
-              <img class="img-fluid" src="@/assets/images/blog_music_techo-1200x600.jpg" alt="">
+              <div class="position-relative">
+                <img class="img-fluid" src="@/assets/images/blog_music_techo-1200x600.jpg" alt="">
+                <div class="start-0 bottom-0 w-100 position-absolute">
+                  <h3>tecnology and music</h3>
+                  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore, quae numquam molestiae nobis magnam animi dolores.</p>
+                </div>
+                <div class="position-absolute top-0 h-100 w-100 ae-flex-center-element">
+                  <h3>tecnology and music</h3>
+                </div>
+              </div>
             </div>
             <div class="col-6">
               <img class="img-fluid" src="@/assets/images/blog-post3-600x900.jpg" alt="">
@@ -51,5 +60,22 @@ export default {
 
  main{
   background-color: $charade;
+  .position-absolute.bottom-0{
+    background-color: $median-charade;
+    padding: 2% 5%;
+    h3{
+    color: $mandy;
+    }
+    p{
+      color: $bombay;
+    }
+  }
+  .position-absolute.h-100{
+    background-color: $design-mandy;
+    h3{
+      color: $white;
+      font-weight: bold;
+    }
+  }  
  }
 </style>
